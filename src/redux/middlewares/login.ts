3a38f34da9b32b8615e31029cecaf67694d27b1e
alejandrorvilla <alejandrorvilla07@gmail.com>
2023-02-03 21:55:29 -0500

    Feature(security): completed

--- src/redux/middlewares/login.ts
+++ src/redux/middlewares/login.ts
@@ -1,16 +1,24 @@
 import { AnyAction } from "@reduxjs/toolkit";
 import { call, put } from "redux-saga/effects";
 import * as LoginServices from "../../services/login";
+import UserStorage from "../../utils/UserStorage";
 import * as LoginActions from "../actions/login";
 
 export function* login(action: AnyAction) {
   try {
-    const sessionToken: Promise<string> = yield call(
+    const data: Promise<Record<string, string>> = yield call(
       LoginServices.login,
       action.payload
     );
-    yield put({ type: LoginActions.ManageLogin.type, payload: sessionToken });
+    yield put({ type: LoginActions.ManageLogin.type, payload: data });
   } catch (error) {
     yield put({ type: LoginActions.ManageError.type, payload: error });
+  }
+}
+
+export function* validateSession(action: AnyAction) {
+  const data = UserStorage.getUserInfo();
+  if (data) {
+    yield put({ type: LoginActions.ManageLogin.type, payload: data });
   }
 }
